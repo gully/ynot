5e1df1acd134fa5a09dcc6b1a66689c4e56f85d6
gully <igully@gmail.com>
2020-10-27 13:37:14 -0500

    Run black on the python demo codes

--- ynot/datasets.py
+++ ynot/datasets.py
@@ -16,27 +16,28 @@ class FPADataset(Dataset):
     Only darks are supported right now!
     """
 
-    def __init__(
-        self,
-        source_metadata = None,
-        n_ims=None,
-        **kwargs
-        ):
-        df_all = pd.read_feather('../../zoja/data/J_band_NIRSPEC/calibration_metadata.feather')
-        df_all = df_all[df_all.IMAGETYP == 'flatlampoff']
+    def __init__(self, source_metadata=None, n_ims=None, **kwargs):
+        df_all = pd.read_feather(
+            "../../zoja/data/J_band_NIRSPEC/calibration_metadata.feather"
+        )
+        df_all = df_all[df_all.IMAGETYP == "flatlampoff"]
 
-        df_all.local_path = df_all.local_path.str.replace('../../', '../../zoja/')
+        df_all.local_path = df_all.local_path.str.replace("../../", "../../zoja/")
         list_of_darks = df_all.local_path.values
         if n_ims is None:
             n_ims = len(list_of_darks)
-        self.images = torch.empty(n_ims, 1024, 1024,dtype=torch.float64)
+        self.images = torch.empty(n_ims, 1024, 1024, dtype=torch.float64)
         for i, fn in enumerate(list_of_darks[0:n_ims]):
-            self.images[i,:,:] = torch.from_numpy(fits.open(fn)[0].data).to(torch.float64)
+            self.images[i, :, :] = torch.from_numpy(fits.open(fn)[0].data).to(
+                torch.float64
+            )
 
         # Filter out mis-labeled images
-        device = 'cpu'#'cuda' if torch.cuda.is_available() else 'cpu'
-        device_median = self.images.view((n_ims, 1024*1024)).to(device).median(-1).values.cpu()
-        mask = device_median > 100 # These are all arc-frames with erroneous labels
+        device = "cpu"  #'cuda' if torch.cuda.is_available() else 'cpu'
+        device_median = (
+            self.images.view((n_ims, 1024 * 1024)).to(device).median(-1).values.cpu()
+        )
+        mask = device_median > 100  # These are all arc-frames with erroneous labels
         self.images = self.images[~mask, :, :]
         df_all = df_all.iloc[0:n_ims]
         df_all = df_all[~mask.numpy()].reset_index(drop=True)
@@ -48,15 +49,15 @@ class FPADataset(Dataset):
         exp_times = df_all.ITIME.values
 
         # Combined x'es
-        self.xs = torch.stack((torch.from_numpy(det_temps),
-                             torch.from_numpy(exp_times)),1).to(torch.float64)
+        self.xs = torch.stack(
+            (torch.from_numpy(det_temps), torch.from_numpy(exp_times)), 1
+        ).to(torch.float64)
 
-        #del device_median
-        #torch.cuda.empty_cache()
-
+        # del device_median
+        # torch.cuda.empty_cache()
 
     def __getitem__(self, index):
-        return (self.xs[index,:], self.images[index,:,:])
+        return (self.xs[index, :], self.images[index, :, :])
 
     def __len__(self):
         return len(self.images)
--- ynot/echelle.py
+++ ynot/echelle.py
@@ -6,7 +6,6 @@ import torch
 from torch import nn
 
 
-
 class Echellogram(nn.Module):
     r"""
     A PyTorch layer that provides a parameter set and transformations to model echellograms.
@@ -20,13 +19,22 @@ class Echellogram(nn.Module):
     def __init__(self, task=None):
         super().__init__()
         # To make "a" and "b" real parameters of the model, we need to wrap them with nn.Parameter
-        self.b = nn.Parameter(torch.tensor(5.0, requires_grad=True, dtype=torch.float64))
-        self.a = nn.Parameter(torch.tensor([0.00, 0.50], requires_grad=True, dtype=torch.float64))
+        self.b = nn.Parameter(
+            torch.tensor(5.0, requires_grad=True, dtype=torch.float64)
+        )
+        self.a = nn.Parameter(
+            torch.tensor([0.00, 0.50], requires_grad=True, dtype=torch.float64)
+        )
+
     def forward(self, x):
         # Computes the outputs / predictions
         variable_part = torch.matmul(x, self.a)
-        bias_term = self.b * torch.ones((x.shape[0],1024, 1024), dtype=torch.float64, device='cuda')
+        bias_term = self.b * torch.ones(
+            (x.shape[0], 1024, 1024), dtype=torch.float64, device="cuda"
+        )
 
-        dark_term = variable_part.unsqueeze(1).unsqueeze(1) * torch.ones((x.shape[0],1024, 1024), dtype=torch.float64, device='cuda')
+        dark_term = variable_part.unsqueeze(1).unsqueeze(1) * torch.ones(
+            (x.shape[0], 1024, 1024), dtype=torch.float64, device="cuda"
+        )
 
         return bias_term + dark_term
