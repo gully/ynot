04abd6f57a2baba34fa679d33bdb9274628e7c73
gully <igully@gmail.com>
2020-10-29 13:09:48 -0500

    Starting to implement the model based on previous notebook experiments

--- src/ynot/echelle.py
+++ src/ynot/echelle.py
@@ -10,15 +10,27 @@ class Echellogram(nn.Module):
     r"""
     A PyTorch layer that provides a parameter set and transformations to model echellograms.
 
-    The parameter set depends on the science goal.
-
     Args:
-        task (str): which task to run
+        ybounds (tuple): the y_0 and y_max of the raw echellogram to analyze. Default: (425, 510)
     """
 
-    def __init__(self, task=None):
+    def __init__(
+        self,
+        device = 'cuda',
+        ybounds=(425,510)):
         super().__init__()
-        # To make "a" and "b" real parameters of the model, we need to wrap them with nn.Parameter
+
+        self.device = device
+        self.y0 = ybounds[0]
+        self.ymax = ybounds[1]
+        self.ny = self.ymax = self.y0
+
+        self.nx = 1024
+        self.xvec = torch.arange(0, self.nx, 1.0)
+        self.yvec = torch.arange(0, self.ny, 1.0)
+        self.xx, self.yy = torch.meshgrid(self.xvec, self.yvec)
+
+
         self.b = nn.Parameter(
             torch.tensor(5.0, requires_grad=True, dtype=torch.float64)
         )
@@ -38,3 +50,17 @@ class Echellogram(nn.Module):
         )
 
         return bias_term + dark_term
+
+    def s_of_xy(self, params):
+    r"""
+    Return the along-slit coordinate :math`s` as a function of :math`(x,y)`
+
+    Args:
+        params (torch.tensor or tuple): the coefficents relating s to (x,y).
+    Returns:
+        (torch.tensor): the 2D map of math:`s(x,y)`
+    """
+        '''
+        y0, kk, dy0_dx = params
+        s_out = kk * ( (self.yy - y0) - dy0_dx * self.xx)
+    return s_out
