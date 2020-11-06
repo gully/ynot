1207af186f3a855875c2990d7be9380e028fbcc8
gully <igully@gmail.com>
2020-11-06 10:22:46 -0600

    Implement a simple AB-nod dataset for a hardcoded example FITS file

--- src/ynot/datasets.py
+++ src/ynot/datasets.py
@@ -1,8 +1,8 @@
 import torch
 from torch.utils.data import Dataset
-import ynot
 from astropy.io import fits
-import pandas as pd
+import numpy as np
+import kornia
 
 # custom dataset loader
 class FPADataset(Dataset):
@@ -10,54 +10,42 @@ class FPADataset(Dataset):
     Focal Plane Array dataset
 
     Args:
-        dark_paths (bool): Analyze Darks
-        source_metadata (DataFrame): A dataframe with header info for each file
 
     Only darks are supported right now!
     """
 
-    def __init__(self, source_metadata=None, n_ims=None, **kwargs):
-        df_all = pd.read_feather(
-            "../../zoja/data/J_band_NIRSPEC/calibration_metadata.feather"
-        )
-        df_all = df_all[df_all.IMAGETYP == "flatlampoff"]
+    def __init__(self, ybounds=(425, 510)):
+        """Read in two AB nods"""
+        super().__init__()
 
-        df_all.local_path = df_all.local_path.str.replace("../../", "../../zoja/")
-        list_of_darks = df_all.local_path.values
-        if n_ims is None:
-            n_ims = len(list_of_darks)
-        self.images = torch.empty(n_ims, 1024, 1024, dtype=torch.float64)
-        for i, fn in enumerate(list_of_darks[0:n_ims]):
-            self.images[i, :, :] = torch.from_numpy(fits.open(fn)[0].data).to(
-                torch.float64
-            )
+        nodA_path = '../../zoja/nsdrp/raw/2012-11-27/NS.20121127.49332.fits'
+        nodA_data = fits.open(nodA_path)[0].data.astype(np.float64)
+        nodA = torch.tensor(nodA_data)
 
-        # Filter out mis-labeled images
-        device = "cpu"  #'cuda' if torch.cuda.is_available() else 'cpu'
-        device_median = (
-            self.images.view((n_ims, 1024 * 1024)).to(device).median(-1).values.cpu()
-        )
-        mask = device_median > 100  # These are all arc-frames with erroneous labels
-        self.images = self.images[~mask, :, :]
-        df_all = df_all.iloc[0:n_ims]
-        df_all = df_all[~mask.numpy()].reset_index(drop=True)
+        nodB_path = '../../zoja/nsdrp/raw/2012-11-27/NS.20121127.50726.fits'
+        nodB_data = fits.open(nodB_path)[0].data.astype(np.float64)
+        nodB = torch.tensor(nodB_data)
 
-        # Use the cold head temperature as proxy for detector temp.
-        det_temps = df_all.CRYOTEMP.values
+        # Read in the Bad Pixel mask
+        bad_pixel_mask_path = '../../zoja/ccdproc/reduced/static/bad_pixel_mask.fits'
+        bpm_data = fits.open(bad_pixel_mask_path)[0].data.astype(np.bool)
+        bpm = torch.tensor(bpm_data)
 
-        # Exposure time
-        exp_times = df_all.ITIME.values
+        data_full = torch.stack([nodA, nodB])
 
-        # Combined x'es
-        self.xs = torch.stack(
-            (torch.from_numpy(det_temps), torch.from_numpy(exp_times)), 1
-        ).to(torch.float64)
+        data = data_full[:, ybounds[0]:ybounds[1], :]
+        data = data.permute(0,2,1)
 
-        # del device_median
-        # torch.cuda.empty_cache()
+        self.pixels = data
+        self.ind = torch.tensor([0,1])
+
+        # Inpaint bad pixels.  In the future we will simply neglect these pixels
+        smoothed_data = kornia.filters.median_blur(data_full.unsqueeze(1), (5,5)).squeeze()
+        data_full[:, bpm] = smoothed_data[:, bpm]
+
 
     def __getitem__(self, index):
-        return (self.xs[index, :], self.images[index, :, :])
+        return (elf.ind[index], self.pixels[index])
 
     def __len__(self):
-        return len(self.images)
+        return len(self.pixels[:,0,0])
