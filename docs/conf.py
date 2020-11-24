e10c4da23a053cec170a16f348e6279f1a0c3433
gully <igully@gmail.com>
2020-11-24 14:31:31 -0600

    Rebuild docs

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.githubpages",
     "sphinx.ext.ifconfig",
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
