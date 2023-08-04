5631a38e72633fa672977a316879bd4ec35c20cc
gully <igully@gmail.com>
2023-08-04 16:09:45 -0500

    Switch to the pydata sphinx theme it looks really nice!

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.githubpages",
     "sphinx.ext.ifconfig",
-    'sphinx.ext.viewcode'
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,7 +57,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pyramid"
+# html_theme = "pyramid"
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
